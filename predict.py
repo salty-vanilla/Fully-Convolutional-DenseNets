c9a64843ac89557a087e0bb0bfe5110d86c3c719
salty-vanilla <jacketgardensio@gmail.com>
2017-04-02 16:09:34 +0900

    predictをちょっとupdate

--- predict.py
+++ predict.py
@@ -1,6 +1,13 @@
 from data_generator import DataGenerator
-from densenet_fc import DenseNetFCN
+from keras.models import model_from_json
 
 
 def main():
-    model = DenseNetFCN()
+    model_json = "dense_fcn.json"
+    model_weights = "dense_fcn.hdf5"
+    model = model_from_json(model_json)
+    model.load_weights(model_weights)
+
+
+if __name__ == "__main__":
+    main()

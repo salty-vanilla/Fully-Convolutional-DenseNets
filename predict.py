6c7739927c10284febc6768e4c7513f594fe0df1
salty-vanilla <jacketgardensio@gmail.com>
2017-04-02 16:45:49 +0900

    update predict.py

--- predict.py
+++ predict.py
@@ -1,13 +1,25 @@
 from data_generator import DataGenerator
 from keras.models import model_from_json
+from densenet_fc import DenseNetFCN
+from keras import backend as K
+
+
+width = 224
+height = 224
+channel = 3
+nb_classes = 21
 
 
 def main():
-    model_json = "dense_fcn.json"
-    model_weights = "dense_fcn.hdf5"
-    model = model_from_json(model_json)
-    model.load_weights(model_weights)
+    input_shape = (height, width, channel) if K.image_dim_ordering() == 'tf' \
+        else (channel, height, width)
+    model = DenseNetFCN(input_shape, nb_dense_block=5, growth_rate=16,
+                        nb_layers_per_block=4, upsampling_type='upsampling',
+                        classes=nb_classes, activation='softmax')
 
+    # jsonからロードできない ・・・
+    model_weights = "dense_fcn.hdf5"
+    model.load_weights(model_weights)
 
 if __name__ == "__main__":
     main()
